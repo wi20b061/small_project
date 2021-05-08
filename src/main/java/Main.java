c41d7cd9f4bc55b76aa8fb18e2620eab5b08a70c
Fiona K <fiona5@gmx.at>
2021-05-08 12:16:50 +0200

    first version - SampleController (POST not working)

---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+import com.sun.net.httpserver.HttpServer;
+import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ResourceConfig rc = new ResourceConfig().packages("slm.controllers");
+
+        HttpServer server =
+                JdkHttpServerFactory.createHttpServer(URI.create("http://localhost:8080/"), rc);
+
+        System.out.println("Server started ...");
+
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        server.stop(0);
+
+        System.out.println("Server stopped.");
+
+    }
+}
---
+++ src/main/java/slm/controllers/SampleController.java
@@ -0,0 +1,28 @@
+package slm.controllers;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/api/sample")
+public class SampleController {
+
+    private static int counter=0;
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String SampleMethod(){
+        counter++;
+        System.out.println("Call to GET");
+        return "sample" + counter;
+    }
+    //funktioniert iwie nicht
+    @POST
+    @Path("/{delta}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String SampleMethodWithDelta(@PathParam("delta") int delta){
+        counter = counter + delta;
+        System.out.println("Call to POST");
+        return "sample" + counter;
+    }
+}
